6a852557fc9ed38bb3bbf157daefe5ce8ac8c70c
Huh Hyeongjun <hhj@devent.kr>
2025-08-14 17:31:53 +0900

    fix: update map

--- apps/client/src/pages/map/index.tsx
+++ apps/client/src/pages/map/index.tsx
@@ -50,7 +50,7 @@ export function MapPage() {
             className='h-[calc(100vh-32px)] w-full'
           >
             <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+              attribution=''
               url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
             />
           </MapContainer>
