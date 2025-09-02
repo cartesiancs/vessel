43bfd099583ac08441acbfe2fc519edfeeccfee6
DipokalLab <hhj@devent.kr>
2025-09-02 17:53:38 +0900

    fix: update sidebar design

--- apps/client/src/components/ui/sidebar.tsx
+++ apps/client/src/components/ui/sidebar.tsx
@@ -197,7 +197,7 @@ function Sidebar({
             <SheetTitle>Sidebar</SheetTitle>
             <SheetDescription>Displays the mobile sidebar.</SheetDescription>
           </SheetHeader>
-          <div className='flex h-full w-full flex-col pt-8'>{children}</div>
+          <div className='flex h-full w-full flex-col'>{children}</div>
         </SheetContent>
       </Sheet>
     );
