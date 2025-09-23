bfcd127739247d11d70a672a69222003b0901edb
DipokalLab <hhj@devent.kr>
2025-09-23 18:19:56 +0900

    feat: add python custom node utils

---
+++ packages/custom-node-utils/add_number.py
@@ -0,0 +1,10 @@
+def main(inputs):
+  a = inputs.get("a", 0)
+  b = inputs.get("b", 0)
+
+
+  outputs = {
+    "number": a+b
+  }
+
+  return outputs
---
+++ packages/custom-node-utils/random.py
@@ -0,0 +1,9 @@
+import random
+
+def main(inputs):
+  result = random.random()
+  outputs = {
+    "number": result
+  }
+
+  return outputs
